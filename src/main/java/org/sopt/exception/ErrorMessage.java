6f91c5ccdc59f5ae21ed2a4a8894e738ef2a7a0f
huncozyboy <leejh5838@naver.com>
2025-04-07 17:54:19 +0900

    #2 [FEAT] 오류 enum 메세지 추가

---
+++ src/main/java/org/sopt/exception/ErrorMessage.java
@@ -0,0 +1,17 @@
+package org.sopt.exception;
+
+public enum ErrorMessage {
+    INVALID_TITLE_LENGTH("게시글 제목은 30자 이하여야 합니다."),
+    NOT_EMPTY_TITLE("게시글 제목은 비어있을 수 없습니다."),
+    DUPLICATED_TITLE("이미 존재하는 게시글 제목입니다.");
+
+    private String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
