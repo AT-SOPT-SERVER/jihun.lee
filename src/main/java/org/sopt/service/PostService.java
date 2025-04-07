470347502ad7fde7adf577ffeed089ff8d29a8df
huncozyboy <leejh5838@naver.com>
2025-04-07 16:23:45 +0900

    [FEAT] Post 서비스 구현

---
+++ src/main/java/org/sopt/service/PostService.java
@@ -0,0 +1,30 @@
+package org.sopt.service;
+
+import java.util.List;
+import org.sopt.domain.Post;
+import org.sopt.dto.PostCreateRequestDto;
+import org.sopt.repository.PostRepository;
+
+public class PostService {
+    private final PostRepository postRepository = new PostRepository();
+
+    public void createPost(String title) {
+        PostCreateRequestDto dto = new PostCreateRequestDto(title);
+        int id = postRepository.nextId();
+        Post post = Post.create(dto, id);
+        postRepository.save(post);
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(int id) {
+        return postRepository.findById(id)
+                .orElse(null);
+    }
+
+    public boolean deletePostById(int id) {
+        return postRepository.deleteById(id);
+    }
+}
