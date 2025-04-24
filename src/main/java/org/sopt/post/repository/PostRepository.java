005a910d1df42270c22577437fd5a5950f4b1cb0
huncozyboy <leejh5838@naver.com>
2025-04-24 16:50:12 +0900

    #4 [FEAT] 가장 최신 게시물 조회하는 findLatestModifiedAt 메서드 레포에 구현

--- src/main/java/org/sopt/post/repository/PostRepository.java
+++ src/main/java/org/sopt/post/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package org.sopt.post.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.sopt.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,5 +14,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     boolean existsByTitle(String title);
 
     List<Post> findAllByTitleContaining(String keyword);
+
+    @Query("select p.modifiedAt from Post p order by p.modifiedAt desc")
+    Optional<LocalDateTime> findLatestModifiedAt();
 
 }
