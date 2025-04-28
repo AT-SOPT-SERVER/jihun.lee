9389ed038b1df3de350d2c2e74725e1129956bf6
huncozyboy <leejh5838@naver.com>
2025-04-29 06:17:14 +0900

    #6 [FEAT] 태그관련 예외처리 구현

--- src/main/java/org/sopt/post/exception/ErrorMessage.java
+++ src/main/java/org/sopt/post/exception/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     NOT_EMPTY_CONTENT("게시글 내용은 비어있을 수 없습니다."),
     DUPLICATED_TITLE("이미 존재하는 게시글 제목입니다."),
     POST_CREATION_INTERVAL_EXCEEDED("새로운 게시글 작성은 3분 간격으로 가능합니다."),
-    NOT_FOUND_POST("해당 ID의 게시글이 존재하지 않습니다.");
+    NOT_FOUND_POST("해당 ID의 게시글이 존재하지 않습니다."),
+    NOT_FOUND_TAG("해당 태그가 존재하지 않습니다.");
 
     private final String message;
 
