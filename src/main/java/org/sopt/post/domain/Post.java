35c0bacdbfcbc94349fccfe1a95091ee257bb8c1
huncozyboy <leejh5838@naver.com>
2025-04-30 00:44:05 +0900

    #6 [MOD] 추가된 태그관련 필드들 엔티티, dto에 반영

--- src/main/java/org/sopt/post/domain/Post.java
+++ src/main/java/org/sopt/post/domain/Post.java
@@ -1,10 +1,20 @@
 package org.sopt.post.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import org.sopt.global.common.entity.BaseEntity;
+import org.sopt.post.domain.enums.Tags;
+import org.sopt.user.domain.User;
 
 @Entity
 public class Post extends BaseEntity {
@@ -13,16 +23,39 @@ public class Post extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User author;
+
     private String title;
 
-    public Post() { }
+    @Lob
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
 
-    public Post(String title) {
+    @Enumerated(EnumType.STRING)
+    private Tags tags;
+
+    protected Post() {}
+
+    public Post(String title, String content, Tags tags, User author) {
         this.title = title;
+        this.content = content;
+        this.tags = tags;
+        this.author = author;
     }
 
     public void updateTitle(String newTitle) {
         this.title = newTitle;
+    }
+
+    public void updateContent(String newContent) {
+        this.content = newContent;
+    }
+
+    public void updateTags(Tags newTag) {
+        this.tags = newTag;
     }
 
     public Long getId() {
@@ -31,6 +64,18 @@ public class Post extends BaseEntity {
 
     public String getTitle() {
         return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Tags getTag() {
+        return tags;
     }
 
 }
