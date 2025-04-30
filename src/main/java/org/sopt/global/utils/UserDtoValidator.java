c86b4d8062fba50f33976c81f9d83dae9a1aed2c
huncozyboy <leejh5838@naver.com>
2025-05-01 01:05:17 +0900

    #6 [FEAT] 유저 DTO 검증 메서드 구현

--- src/main/java/org/sopt/global/utils/UserDtoValidator.java
+++ src/main/java/org/sopt/global/utils/UserDtoValidator.java
@@ -1,28 +1,21 @@
-package org.sopt.user.service;
+package org.sopt.global.utils;
 
-import org.sopt.user.exception.DuplicatedNickNameException;
 import org.sopt.user.exception.EmptyNicknameException;
 import org.sopt.user.exception.InvalidNicknameLengthException;
-import org.sopt.user.repository.UserRepository;
 
-public class UserValidator {
+public class UserDtoValidator {
 
     private static final int MAX_NICKNAME_LENGTH = 10;
 
-    public static void validateNickname(String nickname, UserRepository userRepository) {
+    public static void validateNicknameStructure(String nickname) {
         if (nickname == null || nickname.isBlank()) {
             throw new EmptyNicknameException();
-        }
-
-        if (userRepository.existsByNickname(nickname)) {
-            throw new DuplicatedNickNameException();
         }
 
         int length = nickname.length();
         if (length > MAX_NICKNAME_LENGTH) {
             throw new InvalidNicknameLengthException();
         }
-
     }
 
 }
