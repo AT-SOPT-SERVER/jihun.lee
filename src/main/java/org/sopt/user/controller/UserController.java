16700363fd03a05d3e9bccc3da5c567df190eeb1
huncozyboy <leejh5838@naver.com>
2025-05-01 01:07:51 +0900

    #6 [FEAT] 유저 회원가입 기능 구현

--- src/main/java/org/sopt/user/controller/UserController.java
+++ src/main/java/org/sopt/user/controller/UserController.java
@@ -1,16 +1,17 @@
 package org.sopt.user.controller;
 
 import org.sopt.global.common.response.ApiResponse;
-import org.sopt.user.domain.User;
 import org.sopt.user.dto.UserRequestDto;
 import org.sopt.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
@@ -19,11 +20,11 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user")
-    public ResponseEntity<ApiResponse<User>> createUser(@RequestBody UserRequestDto.Create request) {
-        User createdUser = userService.createUser(request);
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> createUser(@RequestBody UserRequestDto.Create request) {
+        userService.createUser(request);
 
-        return ApiResponse.response(HttpStatus.CREATED, ResponseMessage.USER_CREATE_SUCCESS.getMessage(), createdUser);
+        return ApiResponse.response(HttpStatus.CREATED, ResponseMessage.USER_CREATE_SUCCESS.getMessage());
     }
 
 }
---
+++ src/main/java/org/sopt/user/service/UserService.java
@@ -0,0 +1,29 @@
+package org.sopt.user.service;
+
+import org.sopt.user.domain.User;
+import org.sopt.user.dto.UserRequestDto;
+import org.sopt.user.exception.DuplicatedNickNameException;
+import org.sopt.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void createUser(UserRequestDto.Create dto) {
+        if (userRepository.existsByNickname(dto.nickname())) {
+            throw new DuplicatedNickNameException();
+        }
+
+        User user = User.of(dto.nickname());
+        userRepository.save(user);
+    }
+
+}
