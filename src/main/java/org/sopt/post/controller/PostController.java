ed3bc6148df4e2c10b101f144019b3424079d8e8
huncozyboy <leejh5838@naver.com>
2025-05-01 01:06:17 +0900

    #6 [MOD] 검색 쿼리 최적화해서 구현

--- src/main/java/org/sopt/post/controller/PostController.java
+++ src/main/java/org/sopt/post/controller/PostController.java
@@ -3,7 +3,12 @@ package org.sopt.post.controller;
 import java.util.List;
 import org.sopt.global.common.response.ApiResponse;
 import org.sopt.post.domain.Post;
-import org.sopt.post.dto.PostRequestDto;
+import org.sopt.post.dto.request.PostCreateRequest;
+import org.sopt.post.dto.request.PostDeleteRequest;
+import org.sopt.post.dto.request.PostSearchRequest;
+import org.sopt.post.dto.request.PostUpdateRequest;
+import org.sopt.post.dto.response.PostDetailResponse;
+import org.sopt.post.dto.response.PostSummaryResponse;
 import org.sopt.post.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,43 +34,40 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Post>> createPost(@RequestHeader Long userId, @RequestBody PostRequestDto.Create dto){
+    public ResponseEntity<ApiResponse<Post>> createPost(@RequestHeader final Long userId, @RequestBody PostCreateRequest.Create dto){
         postService.createPost(dto, userId);
 
         return ApiResponse.response(HttpStatus.CREATED, ResponseMessage.POST_CREATE_SUCCESS.getMessage());
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<Post>>> getAllPosts() {
-        List<Post> list = postService.getAllPosts();
+    public ResponseEntity<ApiResponse<List<PostSummaryResponse.Summary>>> getAllPosts() {
 
-        return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_GET_ALL_SUCCESS.getMessage(), list);
+        return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_GET_ALL_SUCCESS.getMessage(), postService.getAllPosts());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<Post>> getPostById(@PathVariable Long id) {
-        Post post = postService.getPostById(id);
+    public ResponseEntity<ApiResponse<PostDetailResponse.Detail>> getPostById(@PathVariable final Long id) {
 
-        return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_GET_DETAIL_SUCCESS.getMessage(), post);
+        return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_GET_DETAIL_SUCCESS.getMessage(), postService.getPostById(id));
     }
 
     @GetMapping("/search")
-    public ResponseEntity<ApiResponse<List<Post>>> searchPosts(@RequestParam String keyword) {
-        List<Post> result = postService.searchPosts(new PostRequestDto.Search(keyword));
+    public ResponseEntity<ApiResponse<List<Post>>> searchPosts(@RequestParam(required = false) final String keyword, @RequestParam(required = false) final String tag) {
 
-        return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_SEARCH_SUCCESS.getMessage(), result);
+        return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_SEARCH_SUCCESS.getMessage(), postService.searchPosts(PostSearchRequest.Search.of(keyword, tag)));
     }
 
     @PatchMapping("/{id}")
-        public ResponseEntity<ApiResponse<Post>> updatePost(@RequestHeader Long userId, @PathVariable Long id, @RequestBody PostRequestDto.Update dto) {
+        public ResponseEntity<ApiResponse<Post>> updatePost(@RequestHeader final Long userId, @PathVariable final Long id, @RequestBody PostUpdateRequest.Update dto) {
         postService.updatePost(userId, id, dto);
 
         return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_UPDATE_SUCCESS.getMessage());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<Void>> deletePostById(@PathVariable Long id) {
-        postService.deletePostById(new PostRequestDto.Delete(id));
+    public ResponseEntity<ApiResponse<Void>> deletePostById(@PathVariable final Long id) {
+        postService.deletePostById(new PostDeleteRequest.Delete(id));
 
         return ApiResponse.response(HttpStatus.OK, ResponseMessage.POST_DELETE_SUCCESS.getMessage());
     }
