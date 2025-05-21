d6450984c6950b588d92559e8f7e6a30edcf5fe9
huncozyboy <leejh5838@naver.com>
2025-05-21 10:44:24 +0900

    #9 [MOD] Getter 어노테이션 적용

--- src/main/java/org/sopt/global/common/entity/BaseEntity.java
+++ src/main/java/org/sopt/global/common/entity/BaseEntity.java
@@ -3,10 +3,12 @@ package org.sopt.global.common.entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
@@ -15,12 +17,4 @@ public abstract class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime modifiedAt;
-
-    public LocalDateTime getModifiedAt() {
-        return modifiedAt;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
 }
