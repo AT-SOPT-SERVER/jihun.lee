8b9de92fb2e2649d0fe1c40bc4dd1b7eaa9d2e3c
huncozyboy <leejh5838@naver.com>
2025-05-23 01:42:13 +0900

    #9 [DEL] 미사용 쿼리문 제거

--- src/main/java/org/sopt/post/repository/PostRepository.java
+++ src/main/java/org/sopt/post/repository/PostRepository.java
@@ -1,9 +1,7 @@
 package org.sopt.post.repository;
 
-import java.util.List;
 import java.util.Optional;
 import org.sopt.post.domain.Post;
-import org.sopt.post.domain.enums.Tags;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,18 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     boolean existsByTitle(String title);
-
-    @Query("""
-    SELECT p
-      FROM Post p
-      JOIN p.tags t
-     WHERE t IN :tags
-    GROUP BY p.id
-    HAVING COUNT(DISTINCT t) = :#{#tags.size()}
-    """)
-    List<Post> findByTagsIn(@Param("tags") List<Tags> tags);
-
-    List<Post> findAllByTitleContainingIgnoreCaseOrAuthorNicknameContainingIgnoreCase(String titleKeyword, String authorKeyword);
 
     Optional<Post> findFirstByAuthorIdOrderByModifiedAtDesc(Long authorId);
 
