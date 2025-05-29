2e4605a683359c3712a69b68e783926bd415ed14
huncozyboy <leejh5838@naver.com>
2025-05-29 15:25:05 +0900

    #9 [FEAT] 댓글 좋아요 엔티티 추가

---
+++ src/main/java/org/sopt/like/domain/CommentLike.java
@@ -0,0 +1,32 @@
+package org.sopt.like.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@Entity
+@Table(name = "comment_likes",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"comment_id","user_id"}))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "comment_id", nullable = false)
+    private Long commentId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+}
