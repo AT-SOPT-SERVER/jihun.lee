932eed9b9aaa9a525e4ab9a882a363c9d5a2aed8
huncozyboy <leejh5838@naver.com>
2025-05-29 13:34:23 +0900

    #9 [FEAT] 게시글 좋아요 엔티티 추가

---
+++ src/main/java/org/sopt/like/domain/PostLike.java
@@ -0,0 +1,32 @@
+package org.sopt.like.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@Entity
+@Table(name = "post_likes",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"post_id","user_id"}))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+}
