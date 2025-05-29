10c310843ab44a81f128011727ba8115a8bb6fa3
huncozyboy <leejh5838@naver.com>
2025-05-29 23:15:02 +0900

    #9 [FIX] 올바른 예외처리 메서드 반환

--- src/main/java/org/sopt/global/common/cache/exception/CacheNotFoundException.java
+++ src/main/java/org/sopt/global/common/cache/exception/CacheNotFoundException.java
@@ -3,10 +3,9 @@ package org.sopt.global.common.cache.exception;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import org.sopt.global.common.exception.BaseException;
-import org.sopt.post.exception.ErrorMessage;
 
 public class CacheNotFoundException extends BaseException {
   public CacheNotFoundException() {
-    super(NOT_FOUND, ErrorMessage.DUPLICATED_TITLE.getMessage());
+    super(NOT_FOUND, ErrorMessage.CACHE_NOT_FOUND.getMessage());
   }
 }
